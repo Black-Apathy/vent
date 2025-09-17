caaf1467de285b9fbdd63d8d8b7c8cc133a33e12
Deril Chundakal <derilchundakal@gmail.com>
2025-09-17 17:57:21 +0530

    Changed db to pass logs in async function

--- server/db.js
+++ server/db.js
@@ -14,13 +14,14 @@ const pool = mysql.createPool({
 });
 
 // Test connection
-pool.getConnection((err, connection) => {
-  if (err) {
+(async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("✅ Connected to the database");
+    connection.release();
+  } catch (err) {
     console.error("❌ Error connecting to the database:", err.message);
-    return;
   }
-  console.log("✅ Connected to the database");
-  connection.release();
-});
+})();
 
 module.exports = pool;
